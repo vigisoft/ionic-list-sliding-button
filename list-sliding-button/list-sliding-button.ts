af9504a87fd51a9625d4c5611c2cd65dd6c07a6e
Guilherme Roberge Daleffe <daleffe@users.noreply.github.com>
2022-12-15 23:06:29 -0300

    First commit

---
+++ list-sliding-button/list-sliding-button.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+
+export type ListSlidingButtonColors = 'primary' | 'secondary' | 'tertiary' | 'success' | 'warning' | 'danger' | 'dark' | 'medium' | 'light';
+
+@Component({
+  selector: 'list-sliding-button',
+  templateUrl: './list-sliding-button.html',
+  styleUrls: ['./list-sliding-button.scss'],
+})
+export class ListSlidingButton {
+
+  @Input('optionColor') optionColor: ListSlidingButtonColors = "primary";
+
+  @Input('icon') icon: string = "";
+  @Input('label') label: string = "";
+
+  constructor() { }
+}
